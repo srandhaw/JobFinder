30f72af4fed2267bb674f8c7e3e8953470acc91e
Sehajbir Randhawa <srandhaw@purdue.edu>
2019-01-02 21:45:23 +0400

    basic screen heirarchy made

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import {createAppContainer, createBottomTabNavigator} from 'react-navigation'
+import {createAppContainer, createBottomTabNavigator, createStackNavigator,} from 'react-navigation'
 import AuthScreen from './screens/AuthScreen'
 import WelcomeScreen from './screens/WelcomeScreen'
+import MapScreen from './screens/MapScreen'
+import DeckScreen from './screens/DeckScreen'
+import SettingsScreen from './screens/SettingsScreen'
+import ReviewScreen from './screens/ReviewScreen'
 
 const MainNavigator = createBottomTabNavigator({
       welcome: {screen: WelcomeScreen},
       auth: {screen: AuthScreen},
+      main: {
+        screen: createBottomTabNavigator({
+           map: {screen: MapScreen},
+           deck: {screen: DeckScreen},
+           review: {
+             screen: createStackNavigator({
+                  review: ReviewScreen,
+                  settings: SettingsScreen,
+             },
+             {
+    initialRouteName: "review"
+            }
+             )
+           }
+        })
+      },
     })
 
 const AppContainer = createAppContainer(MainNavigator);
@@ -15,7 +35,7 @@ const AppContainer = createAppContainer(MainNavigator);
   render() {
       return (
       <View style={styles.container}>
-        <AppContainer/>
+       
       </View>
     );
   }
