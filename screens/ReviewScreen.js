e4b7c5dc9aedc8b2d4d19cfa86d52e91c9faa645
Sehajbir Randhawa <srandhaw@purdue.edu>
2019-01-03 13:06:11 +0400

    styling nav bar

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, Platform} from 'react-native'
 import {Button} from 'react-native-elements'
 
 class ReviewScreen extends Component{
@@ -8,10 +8,17 @@ class ReviewScreen extends Component{
    return {
       title: 'Review Jobs',
       headerRight:
+      (
       <Button
         title="Settings"
         onPress={() => navigation.navigate('settings')}
+        backgroundColor='rgba(0,0,0,0)'
+        color = 'rgba(0,122,255,1)'
       />
+      ),
+      headerStyle: {
+          marginTop: Platform.OS === 'android' ? 24 : 0
+      }
    };
  }
 
