8b04eca0f4d620fe55741cdd38bf1740bd570cce
Sehajbir Randhawa <srandhaw@purdue.edu>
2019-01-08 19:24:55 -0500

    basic implementation of cards done

--- screens/DeckScreen.js
+++ screens/DeckScreen.js
@@ -1,14 +1,44 @@
 import React, {Component} from 'react'
 import {View, Text} from 'react-native'
 import {connect} from 'react-redux'
+import {MapView} from 'expo'
+import {Card, Button } from 'react-native-elements'
 import Swipe from '../components/Swipe'
 
 class DeckScreen extends Component{
+
+    renderCard(job){
+        return(
+            <Card title = {job.jobtitle}>
+
+            <View style = {styles.detailWrapper}>
+               <Text>{job.company}</Text>
+               <Text>{job.formattedRelativeTime}</Text>
+            </View>
+
+            <Text>
+              {job.snippet.replace(/<b>/g,'').replace(/<\/b>/g,'')}
+            </Text>
+
+            </Card>
+        )
+    }
+
+    renderNoMoreCards(){
+        return(
+            <Card title = "No More Cards!">
+            </Card>
+        )
+    }
+
+
     render(){
         return(
             <View>
             <Swipe 
-            data = {this.state.jobs}
+            data = {this.props.jobs}
+            renderCard = {this.renderCard}
+            renderNoMoreCards = {this.renderNoMoreCards}
             />
             
             </View>
@@ -19,6 +49,14 @@ class DeckScreen extends Component{
 const mapStateToProps = (state) => {
     return {
         jobs: state.jobs.results
+    }
+}
+
+const styles = {
+    detailWrapper: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        marginBotton: 10,
     }
 }
 
